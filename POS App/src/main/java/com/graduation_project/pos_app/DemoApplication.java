4d624f281c4f6f7e1179b64cb6235e89fb89bdd1
Amro Baira <amr.baira@gmail.com>
2025-05-26 14:58:35 +0300

    denesting the project again

--- POS App/src/main/java/com/graduation_project/pos_app/DemoApplication.java
+++ POS App/src/main/java/com/graduation_project/pos_app/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.graduation_project.pos_app.demo;
+package com.graduation_project.pos_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
