9e6ce9511bc58943a3b5c18bd076509aa357e02e
omar dorbaa <omar.dev@epsilonsoft.tech>
2025-07-02 23:30:41 +0400

    add 5173 port to allowed origins

--- Central App/src/main/java/com/graduationProject/_thYear/Auth/config/SecurityConfiguration.java
+++ Central App/src/main/java/com/graduationProject/_thYear/Auth/config/SecurityConfiguration.java
@@ -62,7 +62,7 @@ public class SecurityConfiguration {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
--- Central App/src/main/java/com/graduationProject/_thYear/Configuration/CorsConfig.java
+++ Central App/src/main/java/com/graduationProject/_thYear/Configuration/CorsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.util.Arrays;
 
 @Configuration
 public class CorsConfig {
@@ -15,7 +16,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200") // Allow all origins
+                        .allowedOrigins("http://localhost:4200","http://localhost:5173") // Allow all origins
                         .allowedMethods("*") // Allow all methods (GET, POST, etc.)
                         .allowedHeaders("*"); // Allow all headers
             }
