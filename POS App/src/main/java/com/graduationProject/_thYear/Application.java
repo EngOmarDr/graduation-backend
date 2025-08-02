d86e7b36e9787018a82ae9957f5262c301d541a0
Amro Baira <amr.baira@gmail.com>
2025-08-02 14:02:31 +0300

    stupid nigger code code

--- POS App/src/main/java/com/graduationProject/_thYear/Application.java
+++ POS App/src/main/java/com/graduationProject/_thYear/Application.java
@@ -1,6 +1,8 @@
 package com.graduationProject._thYear;
 
 
+import java.util.Map;
+
 import org.apache.kafka.clients.NetworkClient;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
@@ -9,7 +11,10 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaResourceFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
@@ -20,11 +25,27 @@ public class Application {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 		System.out.println(context.getBeansOfType(KafkaAdmin.class));
 		System.out.println(context.getBeansOfType(KafkaAdmin.class).size());
+		System.out.println(context.getBeansOfType(KafkaResourceFactory.class));
+		System.out.println(context.getBeansOfType(KafkaResourceFactory.class).size());
+		
 		var kafkaAdmin = context.getBeansOfType(KafkaAdmin.class).get("kafkaAdmin");
+		var kafkaConsumer = context.getBeansOfType(DefaultKafkaConsumerFactory.class).get("kafkaConsumerFactory");
+		var kafkaProducer = context.getBeansOfType(DefaultKafkaProducerFactory.class).get("kafkaProducerFactory");
 		System.out.println(kafkaAdmin.getConfigurationProperties());
+		System.out.println(kafkaConsumer.getConfigurationProperties());
+		System.out.println(kafkaProducer.getConfigurationProperties());
+
+
+		var map = Map.of("bootstrap.servers","localhost:9093");
+		kafkaAdmin.setBootstrapServersSupplier(null);
 		kafkaAdmin.createOrModifyTopics(TopicBuilder.name("topic3")
-                .partitions(1)
-                .replicas(1)
+				.partitions(1)
+				.replicas(1)
+                .build());
+
+				kafkaAdmin.createOrModifyTopics(TopicBuilder.name("topic44")
+				.partitions(1)
+				.replicas(1)
                 .build());
 	}
 
